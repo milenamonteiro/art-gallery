4e341711057d6309b2c3f2a26a09cd7480613702
DIVYA-19 <41481377+DIVYA-19@users.noreply.github.com>
2019-12-11 22:46:02 +0530

    Update models.py

--- artGallery/post/models.py
+++ artGallery/post/models.py
@@ -28,4 +28,18 @@ class Post(models.Model):
     category = models.PositiveSmallIntegerField(
         choices=CATEGORY,
     )
-    # need to create user_id field
+    user_id = models.ForeignKey(User)          # need to create user_id field
+
+    class User(object):
+        """user model"""
+        
+        user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+        name = models.CharField(max_length=30,null=False,blank=False)
+        email = models.EmailField(max_length=254,unique=True)
+        GENDER_CHOICES = (
+            ('M', 'Male'), 
+            ('F', 'Female'), 
+            ('O', 'Other'))
+        gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+        date_of_birth = models.DateField(max_length=8)
+        created_at = models.DateTimeField(auto_now_add=True)

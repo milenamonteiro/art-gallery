3d93405478694d71cafc0d78789cb0d1b33b26a3
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-12-16 19:39:06 -0300

    frontend packages

---
+++ artgallery/.venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\milen\documents\a minha pasta\projetos\art-gallery\artgallery\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

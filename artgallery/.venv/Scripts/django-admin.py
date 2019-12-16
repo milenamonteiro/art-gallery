ab76ae5972aa867beacc35c3c96c11588e5267ff
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-12-16 19:43:31 -0300

    Fixing files

--- artgallery/.venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\milen\documents\a minha pasta\projetos\art-gallery\artgallery\.venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- artgallery/post/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from post.models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
